b014a76e55aa8d3c77e7efd32ad959481684bd8d
Jonathan Regis <ekoungouloujonathan@gmail.com>
2023-04-15 16:23:33 +0000

    added shipments and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShipmentModule } from './shipment/shipment.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ShipmentModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
