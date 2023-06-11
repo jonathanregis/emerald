047b7e6a0ef0c6a72696985122b31e817090ea60
Jonathan Regis <ekoungouloujonathan@gmail.com>
2023-06-11 08:29:02 +0000

    use env for db credentials

--- src/database/database.ts
+++ src/database/database.ts
@@ -12,11 +12,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'mysql',
-        host: 'localhost',
-        port: 8889,
-        username: 'root',
-        password: 'root',
-        database: 'emerald',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
       });
       sequelize.addModels([
         User,
