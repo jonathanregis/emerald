781f4d78e7d4a061596b10b748919147a166b67c
Jonathan Regis <ekoungouloujonathan@gmail.com>
2023-06-11 09:19:29 +0000

    added deploy script and dynamic port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3001);
+  await app.listen(process.env.APP_PORT || 3001);
 }
 bootstrap();
