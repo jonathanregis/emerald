0eda8905357494966c36580c1f93eb91aa133e59
Jonathan Regis <ekoungouloujonathan@gmail.com>
2023-07-18 10:09:51 +0000

    acme challenge setup

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { WsAdapter } from '@nestjs/platform-ws';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   const config = new DocumentBuilder()
@@ -18,7 +20,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.useWebSocketAdapter(new WsAdapter(app)); // Add this line
-  await app.listen(process.env.APP_PORT || 3001);
+  app.useWebSocketAdapter(new WsAdapter(app));
+  app.useStaticAssets(path.join(__dirname, '..', 'public'));
+  // await app.listen(process.env.APP_PORT || 3001);
+  await app.listen(80);
 }
 bootstrap();
